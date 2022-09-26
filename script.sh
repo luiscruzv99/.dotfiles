7a507d439b2090f5262fc277d1d1b87e20ab8293
Luis C <57672323+luiscruzv99@users.noreply.github.com>
2022-09-26 13:58:45 +0200

    Changed picom version for rounded corners

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trizen -Syu alacritty discord dunst hsetroot htop lapce ly nvidia min nerd-fonts-complete pfetch picom rofi spacefm ttf-font-awesome vscodium ncpamixer wget xorg-fonts-util xorg-fonts-100dpi xorgs-fonts-75dpi xorg-fonts-encondings xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-setxkbmap xorg-x11perf xorg-xauth xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-gamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbutils xorg-xutils xorg-xkiil xorg-xlsclients xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo
+trizen -Syu alacritty discord dunst hsetroot htop lapce ly nvidia min nerd-fonts-complete pfetch picom-ibhagwan-git rofi spacefm ttf-font-awesome vscodium ncpamixer wget xorg-fonts-util xorg-fonts-100dpi xorgs-fonts-75dpi xorg-fonts-encondings xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-setxkbmap xorg-x11perf xorg-xauth xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-gamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbutils xorg-xutils xorg-xkiil xorg-xlsclients xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo
 
 git clone https://github.com/catppuccin/rofi.git
 cd rofi/basic
