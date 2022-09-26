db47a286c8ef1f8e7ff53f629415172a324651c8
Luis C <luiscruzv99@gmail.com>
2022-09-26 11:54:46 +0000

    Added various configs from iKa

--- .bash_profile
+++ .bash_profile
@@ -1,10 +1,11 @@
 #
 # ~/.bash_profile
 #
-
 export LC_TYPE="es_ES.UTF-8"
+export DRI_PRIME=1
 
 if [ "$(tty)" = "/dev/tty2" ]; then
 	exec startx && xsetroot -solid "#ca9ee6"
 fi;		
+
 [[ -f ~/.bashrc ]] && . ~/.bashrc
--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trizen -Syu alacritty lapce ly min nerd-fonts-complete pfetch picom rofi ttf-font-awesome wget xorg-fonts-util xorg-fonts-100dpi xorgs-fonts-75dpi xorg-fonts-encondings xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-setxkbmap xorg-x11perf xorg-xauth xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-gamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbutils xorg-xutils xorg-xkiil xorg-xlsclients xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo
+trizen -Syu alacritty discord dunst hsetroot htop lapce ly nvidia min nerd-fonts-complete pfetch picom rofi spacefm ttf-font-awesome vscodium ncpamixer wget xorg-fonts-util xorg-fonts-100dpi xorgs-fonts-75dpi xorg-fonts-encondings xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-setxkbmap xorg-x11perf xorg-xauth xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-gamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbutils xorg-xutils xorg-xkiil xorg-xlsclients xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo
 
 git clone https://github.com/catppuccin/rofi.git
 cd rofi/basic
@@ -9,3 +9,20 @@ sh install.sh
 cd ../..
 
 git clone https://github.com/luiscruzv99/suckless
+cd suckless/dwm
+make && sudo make install
+cd ../slstatus
+make && sudo make install
+cd ../..
+
+git clone https://github.com/m-bartlett/pavol-dunst.git
+cd pavol-dunst/src
+make && sudo make install
+cd ../..
+
+ln -s ~/.dotfiles/config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/config/dunst ~/.config/dunst
+
+ln -s ~/.dotfiles/.xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/.xbindkeys ~/.xbindkeys
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profiles
