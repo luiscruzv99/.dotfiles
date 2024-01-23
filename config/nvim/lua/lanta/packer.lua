13496d6ab747768eff3abbf9b377c2ecdcd77397
Luis Cruz <luis.cruz@wejyc.com>
2024-01-23 09:55:54 +0100

    Added new plugins

--- config/nvim/lua/lanta/packer.lua
+++ config/nvim/lua/lanta/packer.lua
@@ -1,42 +1,51 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.api.nvim_command, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.api.nvim_command, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  }
-  }
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
+}
 
-  use {'kdheepak/tabline.nvim',
-        requires = { { 'hoob3rt/lualine.nvim', opt=true },
-        {'kyazdani42/nvim-web-devicons', opt = true} }
+use {'kdheepak/tabline.nvim',
+requires = { { 'hoob3rt/lualine.nvim', opt=true },
+{'kyazdani42/nvim-web-devicons', opt = true} }
     }
 
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
+
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+
 end)
--- config/nvim/lua/lanta/set.lua
+++ config/nvim/lua/lanta/set.lua
@@ -1,4 +1,4 @@
-vim.opt.nu = true
+vim.opt.nu=true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -10,15 +10,21 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
-vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-
 vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
+
+vim.opt.foldcolumn = '1'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
